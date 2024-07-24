6985cbf882ff6b9d2bdff923c42870cabb61f007
NkwiCyril <akinimbomnkwi@gmail.com>
2024-07-24 03:31:48 +0100

    creating components

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,16 @@
+import Navbar from "./components/navbar";
+import HeroSection from "./components/heroSection";
+import FeatureSection from "./components/featureSection";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
+      <FeatureSection/>
+    </>
+  );
 }
